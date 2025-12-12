1ad2aaa00d41f9042e367f9fc9f0b26bd811add5
Axel <dsg4857@gmail.com>
2025-12-11 19:20:34 -0500

    change: Cambio de rutas ADMIN

--- src/shared/constants/routes.ts
+++ src/shared/constants/routes.ts
@@ -1,20 +1,26 @@
 export const ROUTES = {
+    //Rutas públicas
     HOME: "/",
     ADS: "/ads",
     AD_DETAIL: (id:string) => `/ads/${id}`,
     CATEGORY: (slug: string) => `/category/${slug}`,
+    CONTACT: "/contact",
+    //RUTA PROTEGIDA
+    CREATE_AD: "/ads/create",
+    // RUTAS DE AUTENTICACION
     AUTH: {
         LOGIN: "/login",
         REGISTER:"/register",
     },
+    // RUTAS DE PERFIL
     PROFILE: {
         ROOT: "/profile",
         MY_ADS: "/profile/my-ads",
     },
+    //RUTAS DE ADMIN
     ADMIN: {
-        ROOT: "/admin",
-        DASHBOARD: "/admin/dashboard",
-        USERS: "/admin/users",
-        CONFIG: "/admin/config",
+        ROOT: "/dashboard",
+        USERS: "/dashboard/users",
+        CONFIG: "/dashboard/config",
     },
 } as const;
